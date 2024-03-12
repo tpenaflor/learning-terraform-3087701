060b963522866ace1cdc471f84826565f1829b09
tyrone.penaflor <tyrone.penaflor@ballys.com>
2024-03-12 16:52:04 -0700

    simplify

--- main.tf
+++ main.tf
@@ -26,12 +26,11 @@ resource "aws_instance" "blog" {
     Name = "HelloWorlds"
   }
 
-  #vpc_security_group_ids = [aws_security_group.blog.id]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 }
 
 module "blog_sg" {
-  name = "blog_new"
+  name = "blog"
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
 
@@ -46,40 +45,3 @@ module "blog_sg" {
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
-
-# resource "aws_security_group" "blog" {
-#   name = "blog"
-#   description = "allow http and https in. allow everythin out"
-
-#   vpc_id = data.aws_vpc.default.id
-# }
-
-# resource "aws_security_group_rule" "blog_http_in" {
-#   type        = "ingress"
-#   from_port   = 80
-#   to_port     = 80
-#   protocol    = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
-
-#   security_group_id = aws_security_group.blog.id
-# }
-
-# resource "aws_security_group_rule" "blog_https_in" {
-#   type        = "ingress"
-#   from_port   = 443
-#   to_port     = 443
-#   protocol    = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
-
-#   security_group_id = aws_security_group.blog.id
-# }
-
-# resource "aws_security_group_rule" "blog_everything_out" {
-#   type        = "egress"
-#   from_port   = 0
-#   to_port     = 0
-#   protocol    = "-1"
-#   cidr_blocks = ["0.0.0.0/0"]
-
-#   security_group_id = aws_security_group.blog.id
-# }

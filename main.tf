acb266982479a5a7247c13f177d9cd26e94ec720
tpenaflor <penaflor.tyrone@gmail.com>
2024-03-12 16:40:35 -0700

    fix typo

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   name = "blog_new"
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2
+  version = "5.1.2"
 
   vpc_id = data.aws_vpc.default.id
 

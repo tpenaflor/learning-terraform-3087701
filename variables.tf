cf0fcd65a4ac25b5f556306e37b09593b02d4715
tyrone.penaflor <tyrone.penaflor@ballys.com>
2024-03-12 17:53:23 -0700

    use variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,41 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description = "name filter and owner for ami"
+  type = object({
+    name = string
+    owner = string
+  })
+
+  default = {
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631"
+  }
+}
+
+variable "environment" {
+  description = "development environment"
+  type = object({
+    name = string
+    network_prefix = string
+  })
+
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "asg_min_size" {
+  description = "min size"
+  type = string
+  default = 1
+}
+
+variable "asg_max_size" {
+  description = "max size"
+  type = string
+  default = 2
+}

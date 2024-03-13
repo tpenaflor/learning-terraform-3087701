8ca7acb824d74191bc8e9c0ee8f74849ff86aae5
tyrone.penaflor <tyrone.penaflor@ballys.com>
2024-03-12 17:01:59 -0700

    add dev vpc

--- main.tf
+++ main.tf
@@ -18,6 +18,20 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "dev"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -27,6 +41,7 @@ resource "aws_instance" "blog" {
   }
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id = module.blog_vpc.public_subnets[0]
 }
 
 module "blog_sg" {
@@ -34,7 +49,8 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
 
-  vpc_id = data.aws_vpc.default.id
+  # vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules = [
     "http-80-tcp",
